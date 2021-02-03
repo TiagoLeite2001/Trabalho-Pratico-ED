d344dad3ba2f4396b61821befc37dc93b1ae5bc5
lopes <lopes.jonas.7@gmail.com>
2021-02-03 22:45:59 +0000

    Updated Jar

--- src/json/JsonExporter.java
+++ src/json/JsonExporter.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package json;
 
-/**
- *
- * @author tiago
- */
+import interfaces.ICenario;
+
 public class JsonExporter {
-    
+     public static String exportSimulacoesManuais(String codMissao,ICenario versao){
+        
+    }
+
 }
